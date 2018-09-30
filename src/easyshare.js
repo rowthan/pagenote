49391e2831c2d974393c1e0fcced3a3b45758804
rhran <912464767@qq.com>
2018-09-30 16:45:59 +0800

    代码重构，文档操作归至document.js

--- src/easyshare.js
+++ src/easyshare.js
@@ -1,47 +1,12 @@
 import {version} from '../package.json'
-import {drawMenu} from './document'
+import {drawMenu,getoPosition,getXY,hightLightElement} from './document'
 import constant from './constant'
 import whatselement from 'whats-element'
 const whats = new whatselement({draw:false})
 const MOUSE_UP = 'ontouchstart' in window
   /* istanbul ignore next */ ? 'touchend'
-  /* istanbul ignore next */ : 'mouseup',
-  IS_TOUCH = 'ontouchstart' in window
+  /* istanbul ignore next */ : 'mouseup'
 
-const getXY = IS_TOUCH
-? e => {
-const touch = e.touches[0] || e.changedTouches[0]
-return touch ? {
-    x: touch.pageX,
-    y: touch.pageY
-} : { x: 0, y: 0 }
-}
-: e => {
-   var e = event || window.event;
-   var x = e.pageX || e.clientX + getScroll().x;
-   var y = e.pageY || e.clientY + getScroll().y;
-   return { 'x': x, 'y': y };
-}
- 
-function getScroll(){
-    var x = document.documentElement.scrollLeft || document.body.scrollLeft;
-    var y = document.documentElement.scrollTop || document.body.scrollTop;
-    return {x,y}
-}
-
-function setScroll(x=0,y=0){
-    document.documentElement.scrollLeft = document.body.scrollLeft = x;
-    document.documentElement.scrollTop =  document.body.scrollTop = y;
-}
-
-function focusOnElement(element){
-
-}
-
-function hightLightElement(element){
-    // console.log("hightligth target")
-    element.style.background = "#e8d2bb"
-}
 
 export default function Easyshare(options={autoReplay:true,hightligth:true}){
     this.options = options
@@ -51,26 +16,27 @@ export default function Easyshare(options={autoReplay:true,hightligth:true}){
 
     window.addEventListener("load", (event)=> {
         const search = window.location.search
-        if(search.indexOf("easyshare")>0){
-            const searchArray = search.substr(1).split("&");
-            for(let i=0 ; i < searchArray.length; i++){
-                const queryPar  = searchArray[i]
-                if(queryPar.indexOf("easyshare")>-1){
-                    const pair = queryPar.split("=");
-                    if(pair.length===2){
-                        const values = pair[1]
-                        const replaySteps = []
-                        values.split("a").forEach(value=>{
-                            const tempStep = {
-                                x:value.split("-")[0],
-                                y:value.split("-")[1]
-                            }
-                            replaySteps.push(tempStep)
-                        })
-                        this.replay(0,replaySteps)
-                    }
-                    break
+        if(search.indexOf("easyshare")===-1){
+            return
+        }
+        const searchArray = search.substr(1).split("&");
+        for(let i=0 ; i < searchArray.length; i++){
+            const queryPar  = searchArray[i]
+            if(queryPar.indexOf("easyshare")>-1){
+                const pair = queryPar.split("=");
+                if(pair.length===2){
+                    const values = pair[1]
+                    const replaySteps = []
+                    values.split("a").forEach(value=>{
+                        const tempStep = {
+                            x:value.split("-")[0],
+                            y:value.split("-")[1]
+                        }
+                        replaySteps.push(tempStep)
+                    })
+                    this.replay(0,replaySteps)
                 }
+                break
             }
         }
     });
@@ -113,18 +79,16 @@ export default function Easyshare(options={autoReplay:true,hightligth:true}){
         history.pushState("", "EasyShare 预览", currentUrl+share);
     }
 
-    
-
     document.addEventListener( MOUSE_UP , (e)=>{
-        const selectd = getText(e);
-        if(selectd.text===targetInfo.text){
+        const selectdText = document.getSelection().toString().trim();
+        if(selectdText===targetInfo.text){
             console.log("不再监听")
             return
         } 
         
         const shareContainer = document.getElementById(constant.EASYSHARECONTAINER)
         //存在选中内容 并且 状态为暂停时 显示锚点按钮 存储相关信息
-        if(selectd.text){
+        if(selectdText){
             const { x, y } = getXY(e)
             shareContainer.style.transform = `translateX(${x}px) translateY(${y}px)`
             shareContainer.style.display = "block"
@@ -133,8 +97,8 @@ export default function Easyshare(options={autoReplay:true,hightligth:true}){
             targetInfo = {
                 x:document.documentElement.scrollLeft,
                 y:document.documentElement.scrollTop,
-                text:selectd.text,
-                id: selectd.id ? selectd.id.wid : null
+                text:selectdText,
+                id: whats.getUniqueId(e.target).wid
             }
         }else{
             targetInfo = {}
@@ -143,7 +107,6 @@ export default function Easyshare(options={autoReplay:true,hightligth:true}){
         }
     } );
     
-
     this.finished = function(){
         this.status = constant.FINNISHED
         this.setps = []
@@ -186,36 +149,6 @@ export default function Easyshare(options={autoReplay:true,hightligth:true}){
         menuEl.innerHTML = value
     })})
     Object.defineProperty(this,"version",{value:version})
-}
-
-
-function getText(e) {
-    const selected = document.getSelection()
-    return {
-        text:selected.toString().trim(),
-        id: whats.getUniqueId(e.target)
-    };
-}
-
-
-function getoPosition(targetX=0,targetY=0,callback){
-    // console.log("target position:",targetX,targetY)
-    const timer = setInterval(function () {
-        //移动前
-        const { x:beforeScrollLeft,y:beforeScrollTop} = getScroll();
-        const distanceX = targetX - beforeScrollLeft
-        , distanceY =  targetY - beforeScrollTop
-        
-        //移动后
-        setScroll(beforeScrollLeft+Math.floor(distanceX/6),Math.floor(beforeScrollTop+distanceY/6))
-        
-        const {x:afterScrollLeft,y:afterScrollTop} = getScroll()
-        
-        if(beforeScrollTop === afterScrollTop && beforeScrollLeft === afterScrollLeft){
-            clearInterval(timer)
-            callback()
-        }
-    },30)
 }
 
 Easyshare.prototype.init = function(){console.log("init")}
