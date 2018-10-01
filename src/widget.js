40a115738d1494e30b70138c944747db0402fe00
rowthan <12638456+rowthan@users.noreply.github.com>
2018-10-01 18:40:27 +0800

    使用hyperapp 构建ui层，释放easyshare职责

--- src/widget.js
+++ src/widget.js
@@ -1,49 +1,48 @@
-/** @jsx h */
-// import { h, render, Component } from 'preact';  // normally this would be an import statement.
-
-// class Clock extends Component {
-//     constructor() {
-//         super();
-//         // set initial time:
-//         this.state.time = Date.now();
-//     }
-
-//     componentDidMount() {
-//         // update time every second
-//         this.timer = setInterval(() => {
-//             this.setState({ time: Date.now() });
-//         }, 1000);
-//     }
-
-//     componentWillUnmount() {
-//         // stop when not renderable
-//         clearInterval(this.timer);
-//     }
-
-//     render(props, state) {
-//         let time = new Date(state.time).toLocaleTimeString();
-//         return <span>{ time }</span>;
-//     }
-// }
-
-// render(<Clock />, document.body);
-
 import {  h,app } from "hyperapp"
-const state = {
-    count: 0
-}
-  
-  const actions = {
-    down: value => state => ({ count: state.count - value }),
-    up: value => state => ({ count: state.count + value })
+export default function widget(easyshare){
+  const state = {
+    status: easyshare.status,
+    recordedSteps: easyshare.recordedSteps,
+    mousePosition :{x:easyshare.mousePosition.x,y:easyshare.mousePosition.y}
   }
-  
-  const view = (state, actions) => (
-    <div oncreate={()=>{const timer = setInterval(()=>actions.up(1),1000)}}>
-      <h1>{state.count}</h1>
-      <button onclick={() => actions.down(1)}>-</button>
-      <button onclick={() => actions.up(1)}>+</button>
-    </div>
+    
+  const actions = {
+    refershState: value => state =>({
+      status: easyshare.status,
+      recordedSteps: easyshare.recordedSteps,
+      mousePosition :{x:easyshare.mousePosition.x,y:easyshare.mousePosition.y}
+    })
+  }
+
+  const record = (actions)=>{
+    easyshare.record()
+    actions.refershState()
+  }
+
+
+  const RecordButton = ({status,onclick}) =>(
+    <button onclick={onclick}>
+      {status}
+    </button>
   )
   
+
+  const view = (state, actions) => (
+    <div oncreate={easyshare.onStateChange = function(){actions.refershState();console.log(state)}}>
+      <div style={{position:"absolute",left:state.mousePosition.x+"px",top:state.mousePosition.y+"px",transition:".5s"}}>
+        {
+          state.status == "WAITING"
+          &&
+          <span>
+            <RecordButton status={state.status} onclick={()=>{record(actions)}}></RecordButton>  
+            <button onclick={easyshare.makelink}>
+              生成链接
+            </button>
+          </span>
+         
+        }
+      </div>
+    </div>
+  )
   app(state, actions, view, document.body)
+}
