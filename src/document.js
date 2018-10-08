a4199ef6bdc42ee4483bad53a39a035b749e06f0
rowthan <912464767@qq.com>
2018-10-08 23:18:31 +0800

    解决滚动兼容性问题
    - 不同设备对 document.body.scrollTop
    document.documentElement.scrollTop -> window.offsetY

--- src/document.js
+++ src/document.js
@@ -1,22 +1,4 @@
-const gotoPosition = function(targetX=0,targetY=0,callback){
-    const timer = setInterval(function () {
-        //移动前
-        const { x:beforeScrollLeft,y:beforeScrollTop} = getScroll();
-        const distanceX = targetX - beforeScrollLeft
-        , distanceY =  targetY - beforeScrollTop
-        
-        //移动后
-        setScroll(beforeScrollLeft+Math.floor(distanceX/6),Math.floor(beforeScrollTop+distanceY/6))
-        
-        const {x:afterScrollLeft,y:afterScrollTop} = getScroll()
-        
-        if(beforeScrollTop === afterScrollTop && beforeScrollLeft === afterScrollLeft){
-            clearInterval(timer)
-            typeof callback === "function" && callback()
-        }
-    },30)
-    return timer
-}
+
 
 const IS_TOUCH = 'ontouchstart' in window,
 //TODO 优化移动设备
@@ -68,16 +50,37 @@ hightLightElement = function (element,text,revert){
 }
 
 
+const gotoPosition = function(targetX=0,targetY=0,callback){
+    const timer = setInterval(function () {
+        //移动前
+        const { x:beforeScrollLeft,y:beforeScrollTop} = getScroll();
+        const distanceX = targetX - beforeScrollLeft
+        , distanceY =  targetY - beforeScrollTop
+        
+        //移动后
+        setScroll(beforeScrollLeft+Math.floor(distanceX/6),Math.floor(beforeScrollTop+distanceY/6))
+        
+        const {x:afterScrollLeft,y:afterScrollTop} = getScroll()
+        
+        if(beforeScrollTop === afterScrollTop && beforeScrollLeft === afterScrollLeft){
+            clearInterval(timer)
+            typeof callback === "function" && callback()
+        }
+    },30)
+    return timer
+}
+
 const documentTarget = document.documentElement || document.body
 function getScroll(){
-    var x = documentTarget.scrollLeft || documentTarget.scrollLeft;
-    var y = documentTarget.scrollTop || documentTarget.scrollTop;
+    var x = window.pageXOffset || documentTarget.scrollLeft || documentTarget.scrollLeft;
+    var y = window.pageYOffset || documentTarget.scrollTop || documentTarget.scrollTop;
     return {x,y}
 }
 
 function setScroll(x=0,y=0){
-    documentTarget.scrollLeft = documentTarget.scrollLeft = x;
-    documentTarget.scrollTop =  documentTarget.scrollTop = y;
+    documentTarget.scrollLeft = x;
+    documentTarget.scrollTop = y;
+    window.scrollTo(x,y)
 }
 
 function getViewPosition(elem) { // crossbrowser version
