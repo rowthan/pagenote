233edd78097c79754e4193cfe2619938bb326098
rowthan <12638456+rowthan@users.noreply.github.com>
2018-10-19 11:33:29 +0800

    :hammer: 通过注入监听器进行状态变更获取

--- extenstion/scripts/content_script.js
+++ extenstion/scripts/content_script.js
@@ -1,1 +1,6 @@
-var easyshare = new EasyShare("yes-container")
+var easyshare = new EasyShare("yes-container")easyshare.addListener (function(status){
+    if(status==10){
+        localStorage.setItem(window.location.host+location.pathname,easyshare.data)
+        console.log(easyshare.data)
+    }
+})
