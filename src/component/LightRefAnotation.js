20c582edad07a0fdd0bf90228548579a1f1ddd7d
rowthan <12638456+rowthan@users.noreply.github.com>
2020-12-27 11:08:35 +0800

    outline 关键词首字母样式提醒

--- src/component/LightRefAnotation.js
+++ src/component/LightRefAnotation.js
@@ -3,10 +3,16 @@ import Delete from "../assets/delete.svg";
 import './LightRefAnotation.scss'
 
 export default  function LightRefAnotation({step}) {
+  const text = step.text||step.tip;
   return(
     <pagenote-ref-anotation data-insign={step.isInview?'1':''}>
       <pagenote-light-ref>
-        {step.text||step.tip}
+        <span style={{color:step.isActive?step.bg:'',fontSize:'1.2em'}}>
+          {text.substr(0,1)}
+        </span>
+        <span>
+          {text.substring(1)}
+        </span>
       </pagenote-light-ref>
       <pagenote-light-anotation dangerouslySetInnerHTML={{__html: step.text!==step.tip?step.tip:''}}>
       </pagenote-light-anotation>
