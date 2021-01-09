692cc6de9ec190a726b7a54b2dc2606066d798f7
rowthan <ranronghuano1@163.com>
2021-01-09 16:51:01 +0800

    通过填充色和边框色区分视野内、高亮状态

--- src/component/LightRefAnotation.js
+++ src/component/LightRefAnotation.js
@@ -1,25 +1,33 @@
 import {h, render, Component, Fragment} from 'preact';
 import Delete from "../assets/delete.svg";
+// import MoreAction from '../assets/more-action.svg'
 import './LightRefAnotation.scss'
 
 export default  function LightRefAnotation({step}) {
   const text = step.text||step.tip;
+  const notion = step.text!==step.tip?step.tip:'';
   return(
-    <pagenote-ref-anotation data-insign={step.isInview?'1':''}>
+    <pagenote-ref-anotation data-insign={step.isInview?'1':''} style={{'--fill-color':(step.isActive&&step.isInview)?step.bg:'#fff'}}>
       <pagenote-light-ref>
-        <span style={{color:step.isActive?step.bg:'',fontSize:'1.2em'}}>
-          {text.substr(0,1)}
-        </span>
         <span>
-          {text.substring(1)}
+          {text.substring(0,28)}
         </span>
       </pagenote-light-ref>
-      <pagenote-light-anotation dangerouslySetInnerHTML={{__html: step.text!==step.tip?step.tip:''}}>
+      <pagenote-light-anotation>
+        {
+          step.isActive && notion &&
+          <pagenote-block>
+            <pagenote-block>
+              {new Date(step.time).toLocaleDateString()}
+            </pagenote-block>
+            <pagenote-block dangerouslySetInnerHTML={{__html: notion}}>
+
+            </pagenote-block>
+          </pagenote-block>
+        }
       </pagenote-light-anotation>
       <pagenote-light-actions>
-        <pagenote-icon>
-          <Delete onClick={(e)=>step.delete(e)}/>
-        </pagenote-icon>
+        <Delete onClick={(e)=>step.delete(e)}/>
       </pagenote-light-actions>
     </pagenote-ref-anotation>
   )
