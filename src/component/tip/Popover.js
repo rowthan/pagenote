1ba32a9b2bb4344e219f36fef3b4c248f1f76eb8
rowthan <12638456+rowthan@users.noreply.github.com>
2021-04-06 21:24:07 +0800

    feat: popover 升级

--- src/component/tip/Popover.js
+++ src/component/tip/Popover.js
@@ -1,22 +1,26 @@
-import {h} from 'preact';
+import {h,createRef} from 'preact';
 import ToolTip from "rc-tooltip";
 import './popover.less'
 
-export default function Tip({message,children,...props}) {
+export default function Popover({message,children,...props}) {
+  const ref = createRef();
   return(
     <ToolTip destroyTooltipOnHide={{ keepParent: false }}
-             align={{
-               offset: [0, 10],
-             }}
+             // align={{
+             //   offset: [0, 10],
+             // }}
              overlayStyle={{zIndex:9999999}}
              prefixCls='rc-popover'
              placement="bottom"
              trigger={['click']}
-             offsetX={10}
+             // offsetX={10}
              overlay={<span>{message}</span>}
+             getTooltipContainer={function (a) {
+                 return a.parentNode
+             }}
              {...props}
       >
-      {children}
+        {children}
     </ToolTip>
   )
 }
