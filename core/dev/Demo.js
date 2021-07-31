9963c9231ccc5bf333005b5e61b033f6228910ec
rowthan <12638456+rowthan@users.noreply.github.com>
2021-07-31 22:58:05 +0800

    feat: 增加 demo

--- core/dev/Demo.js
+++ core/dev/Demo.js
@@ -1,14 +1,76 @@
-
-// import React,{Component} from 'react'
 import {h} from 'preact';
-import './demo.scss'
+import '../demo/index.css'
 export default function Demo() {
     return (
         <div className="demo">
             <article>
                 <section>
                     <h2>
-                        <span className="en">How About PAGENOTE</span>
+                        PAGENOTE SDK 尝鲜功能
+                    </h2>
+                    <div className="block">
+                        <div className="part">
+                            <h3>
+                                这个页面是什么？
+                            </h3>
+                            <p>
+                                你现在访问的是 PAGENOTE SDK 功能预览页面，你不必安装插件也可以试用主要功能。
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <h3>SDK 新功能列表</h3>
+                    <ul>
+                        <li>
+                            <span>4.8.0-typescript。我们对 UI、交互都做了一次大的改造和升级。</span>
+                            <ul>
+                                <li>
+                                    将批注和功能按钮区进行了拆分。
+                                </li>
+                                <li>
+                                    有笔记的高亮末尾会增加一个图标，以提示此处高亮有笔记。
+                                </li>
+                                <li>
+                                    单击高亮区域能切换状态：浅高亮、深高亮；双击可以快速进入编辑状态。
+                                </li>
+                                <li>
+                                    批注的拖拽会更丝滑
+                                </li>
+                                <li>
+                                    通过快捷键来高亮，可以直接进入编辑笔记模式。（选中一段文本后，长按 P 试试）
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3>接下来的新功能规划是什么？</h3>
+                    <ol>
+                        <li>
+                            侧边栏会被去掉。PAGENOTE 会更少的侵入原本网页。减少不必要的元素，影响原网页。
+                        </li>
+                        <li>
+                            会将「长按唤起」升级为「召唤模式」，长按鼠标来激活划词功能，否则不激活。就像数码宝贝变身有没有？
+                        </li>
+                        <li>
+                            开放对 图像、视频、音频的标记功能。并针对选区的不同内容，展示不同的功能按钮。
+                        </li>
+                        <li>
+                            画笔也要分级，除了支持自定义颜色，增加 1、2级区分。
+                        </li>
+                        <li>
+                            暂时就透露这些功能啦，想法还有超级多，一个一个实现吧。
+                        </li>
+                    </ol>
+                </section>
+
+                <hr/>
+
+                <section>
+                    <h2>
                         <span> 你能用 PAGENOTE 做什么？ </span>
                     </h2>
                     <div className="block">
@@ -153,40 +215,23 @@ export default function Demo() {
 
                 <section>
                     <h2>
-                        PAGENOTE 起源？
+                        关于我
                     </h2>
                     <div className="block">
                         <div className="part">
-                            <h3>
-                                网页里留笔记
-                            </h3>
+                            喜欢软件开发的我，需要联系我的话，关注公众号吧：
                             <div>
-                                的初衷是为了能在网页里留一些笔记，能把账号、表单项的填写数据留在网页里，不必单独使用一个记事本，随用随取，不打扰最有效。
-                                还可以把关注的内容高亮，下次打开时提醒自己
+                                <img width="100" height='100' src="https://pagenote.cn/img/wechat.jpg" alt="pagenote"/>
                             </div>
+                            <p>
+                                如果觉得 PAGENOTE 还不错，微信扫一扫，小小打赏一下吧
+                                <br/>
+                                <img width="100" height='100' src="https://pagenote.cn/img/pay/wechat-pay.png" alt=""/>
+                            </p>
                         </div>
                     </div>
                 </section>
             </article>
-            <p id="one">
-                内容内容内容
-            </p>
-            <p>
-                内容内容内容
-            </p>
-            <p className="two">
-                内容内容内容
-            </p>
-            <p>
-                内容内容内容
-            </p>
-            <p>当你在网页上选中一段内容后，你安装的划词插件会立即出现按钮 -- 或许，这已经打扰到你。</p>
-            <p>
-                而 PAGENOTE 允许你灵活控制按钮是否出现：按住鼠标不松一定时间后，PAGENOTE 才会开始工作。
-            </p>
-            <p>
-                你不必注册账号就能管理你的数据，因为数据就在你本机。
-            </p>
         </div>
     )
 }
