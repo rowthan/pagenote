d6b5ae2a162f182130e5cd825323bf020d5e5bf9
rowthan <12638456+rowthan@users.noreply.github.com>
2021-08-18 23:03:28 +0800

    :bookmark: release 4.10.1

--- sdk/src/pagenote-core.js
+++ sdk/src/pagenote-core.js
@@ -410,7 +410,7 @@ export default function PagenoteCore(id, options={}){ // TODO 支持载入语言
     this.record = function(info={},showComment){
         info = Object.assign(this.target,info);
         this.status = constant.RECORDING;
-        if(options.beforeRecord()===false){
+        if(typeof options.beforeRecord === 'function' && options.beforeRecord()===false){
             return;
         }
         const newStep = new Step(info,StepOptions,function (step) {
