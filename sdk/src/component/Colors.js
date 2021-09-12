63485ac21bfc21708838105134ad429d35ecdd0a
rowthan <ranronghuano1@163.com>
2021-09-12 10:08:30 +0800

    feat: close css module

--- sdk/src/component/Colors.js
+++ sdk/src/component/Colors.js
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-import style from "./light/light-node.scss";
+import "./light/light-node.scss";
 import ColorIcon from "../assets/images/color.svg";
 
 export default function Colors({colors,current,selectColor}) {
@@ -7,19 +7,19 @@ export default function Colors({colors,current,selectColor}) {
         selectColor(color);
     };
     return(
-        <div className={style.colors}>
-            <div className={style.colorOptions}>
+        <pagenote-colors>
+            <pagenote-color-items>
                 {
                     colors.map((color)=>{
                         return <pagenote-icon aria-controls='color-item' onClick={() => { setColor(color)}}
-                                              className={`${style.colorItem} ${color === current ? style.active : ''}`}
+                                              data-active={`${color === current ? '1' : ''}`}
                                               style={{backgroundColor: color}}/>
                     })
                 }
-            </div>
+            </pagenote-color-items>
             <pagenote-icon aria-controls='color-item'>
                 <ColorIcon width={20} height={20} fill={current}/>
             </pagenote-icon>
-        </div>
+        </pagenote-colors>
     )
 };
