3bc6895d8acdcf9e895a282c8c7aec05d4514028
rowthan <12638456+rowthan@users.noreply.github.com>
2021-11-14 14:30:01 +0800

    feat: common 增加 addon

---
+++ sdk/src/common/Pconsole.ts
@@ -0,0 +1,31 @@
+
+interface PconsoleOption {
+    showLog: true,
+}
+
+const Pconsole = function (option:PconsoleOption) {
+    this.option = option;
+}
+
+Pconsole.prototype.log = function () {
+    if(this.option.showLog){
+        // @ts-ignore
+        console.log(...arguments)
+    }
+}
+
+Pconsole.prototype.debug = function () {
+    if(this.option.showLog){
+        // @ts-ignore
+        console.log(...arguments)
+    }
+}
+
+Pconsole.prototype.error = function () {
+    if(this.option.showLog){
+        // @ts-ignore
+        console.error(...arguments)
+    }
+}
+
+export default Pconsole
