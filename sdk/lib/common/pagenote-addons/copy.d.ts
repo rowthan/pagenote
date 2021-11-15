b5e931a3b0d79d547c9d18b3ff7b2bb74972c7aa
rowthan <ranronghuano1@163.com>
2021-11-15 20:49:58 +0800

    :bookmark: release 5.3.4

---
+++ sdk/lib/common/pagenote-addons/copy.d.ts
@@ -0,0 +1,11 @@
+declare const copy: {
+    icon: string;
+    id: string;
+    name: string;
+    description: string;
+    scene: string;
+    settings: any[];
+    clickScript: string;
+    defaultSetting: {};
+};
+export default copy;
