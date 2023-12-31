6046958fdc6e4385f45e4c2f9e489bd2bfc774bb
rowthan <12638456+rowthan@users.noreply.github.com>
2023-12-31 13:06:21 +0800

    perf: remove cache

--- apps/web/public/worker-register.js
+++ apps/web/public/worker-register.js
@@ -2,12 +2,7 @@ navigator.serviceWorker
   .register('/sw.js')
   .then(function (registration) {
     // 方法尝试更新service worker
-    registration.update().then(function () {
-        registration.active.postMessage({
-            type: 'add_cache',
-            values: ['/release', '/pro-plan', '/widget/pro-plan'],
-        })
-    })
+    registration.update()
   })
   .catch(function (err) {
     console.log(err)
