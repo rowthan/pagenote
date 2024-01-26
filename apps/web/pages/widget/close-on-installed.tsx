f1e6c17f7c84a766ca2e1811325cc6fb12d6fc48
rowthan <12638456+rowthan@users.noreply.github.com>
2024-01-26 12:32:56 +0000

    feat: tip 360 browser

--- apps/web/pages/widget/close-on-installed.tsx
+++ apps/web/pages/widget/close-on-installed.tsx
@@ -3,30 +3,37 @@ import useVersionValid from 'hooks/useVersionValid'
 import extApi from '@pagenote/shared/lib/pagenote-api'
 import useCurrentTab from 'hooks/useCurrentTab'
 import { useRouter } from 'next/router'
-import dayjs from 'dayjs'
+import useWhoAmi from 'hooks/useWhoAmi'
 
 interface Props {
   children?: ReactNode
 }
 
-const LOG_CLOSE_ON_INSTALLED = 'log_close_on_installed'
 export default function CloseOnInstalled(props: Props) {
   const { children } = props
   const router = useRouter()
+  const [whoAmI] = useWhoAmi();
   const { valid } = useVersionValid(
-    router.query?.version?.toString() || '0.27.0'
+    router.query?.version?.toString() || '0.28.10'
   )
-  const { tab } = useCurrentTab()
+  const { tab } = useCurrentTab();
+
+  useEffect(function(){
+    if(whoAmI && whoAmI.extensionPlatform === "360"){
+      extApi.developer.chrome({
+        "namespace":"tabs",
+        type: "create",
+        args: [{
+          url: "https://pagenote.cn/360"
+        }]
+      })
+    }
+  },[whoAmI])
 
   useEffect(
     function () {
-      const flag = localStorage.getItem(LOG_CLOSE_ON_INSTALLED)
-      // 一天最多只提示一次
-      const today = dayjs().format('YYYY-MM-DD')
-      const closeMe = flag === today || (router.query && valid && tab?.id)
-
+      const closeMe = valid && tab?.id
       if (closeMe) {
-        localStorage.setItem(LOG_CLOSE_ON_INSTALLED, today)
         extApi.developer.chrome({
           namespace: 'tabs',
           type: 'remove',
@@ -34,7 +41,8 @@ export default function CloseOnInstalled(props: Props) {
         })
       }
     },
-    [valid, tab, router.query]
+    [valid, tab]
   )
+
   return <div className="">{children}</div>
 }
