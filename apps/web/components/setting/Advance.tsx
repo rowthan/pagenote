223b573f7eb471a987c0d13daedc093b13b41874
rowthan <12638456+rowthan@users.noreply.github.com>
2024-08-15 08:43:15 +0800

    feat: 指纹识别开关配置

--- apps/web/components/setting/Advance.tsx
+++ apps/web/components/setting/Advance.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import BasicSettingLine, { SettingSection } from './BasicSettingLine'
+import useSettingConfig from "../../hooks/table/useSettingConfig";
+import {Switch} from "../../@/components/ui/switch";
 
 export default function Advance() {
-  return (
+    const [config,update] = useSettingConfig<{
+        webSignature?: boolean,
+    }>('extension','config');
+
+    const enabled = config?.webSignature;
+
+    return (
       <div className={'relative'}>
           <SettingSection>
               <BasicSettingLine
@@ -12,6 +20,17 @@ export default function Advance() {
               <BasicSettingLine
                   label={'自定义样式'}
                   path={'/advance/style'}
+              />
+              <BasicSettingLine
+                  label={'网页指纹（Beta）'}
+                  subLabel={'对网页生成指纹，不同URL指向同一网页的依据'}
+                  right={
+                      <Switch checked={enabled} onCheckedChange={(checked)=>{
+                          update({
+                              webSignature: checked,
+                          })
+                      }} />
+                  }
               />
           </SettingSection>
       </div>
