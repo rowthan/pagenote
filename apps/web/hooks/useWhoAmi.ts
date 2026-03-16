70d7159d2c613bc8a0a721100d38195d287ba175
rowthan <12638456+rowthan@users.noreply.github.com>
2026-03-16 09:29:02 +0800

    fix: 修复服务器后启动重试机制

--- apps/web/hooks/useWhoAmi.ts
+++ apps/web/hooks/useWhoAmi.ts
@@ -12,13 +12,22 @@ export default function useWhoAmi(
   initVersion: string = '0.0.0'
 ): [WhoAmI | undefined | null, boolean] {
   const { data, isLoading, mutate } = useSWR<WhoAmI>('/whoAmI', fetchInfo, {
+    shouldRetryOnError: true,
+    errorRetryCount: 5,
+    errorRetryInterval: 500, // 服务器可能延迟启动
     fallbackData: {
       version: initVersion,
     },
   })
 
   function fetchInfo() {
-    return extApi.user.getWhoAmI().then(function (res) {
+    return extApi.user.getWhoAmI(undefined,{
+      timeout: 100
+    }).then(function (res) {
+      if(res.data === undefined){
+        // 抛出异常，触发重试机制
+        throw Error('')
+      }
       return res.data || {}
     })
   }
